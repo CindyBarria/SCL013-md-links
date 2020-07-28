fd0cf412234889fb53b7768593ee13745e838fb5
EliHidalgo <hidalgo_eliana@hotmail.com>
2020-07-28 11:39:03 -0400

    funcion para validar URL y status

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const md = require('markdown-it')();
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const chalk = require('chalk');
+/* const fetch = require('fetch');
+const fetchLink = fetch.fetchUrl; */
+const nodeFetch = require('node-fetch');
 
 const verifyMdFile = (dom, path) => {
   const links  = dom.window.document.querySelectorAll('a');
@@ -19,7 +22,25 @@ const verifyMdFile = (dom, path) => {
     }
   })
   console.log(linkObjects);
+  validateUrl(linkObjects);
 }
+
+const validateUrl = (links) => { 
+  links.map(link => {
+    nodeFetch(link.href)
+    .then(resp => {
+      if(resp.status === 200) {
+        console.log(chalk.greenBright(`Link WORKING, status: ${resp.status} ${link.href}`))
+      } else {
+        console.log(chalk.redBright(`Link BROKEN, status: ${resp.status} ${link.href}`))
+      }
+    })
+    .catch(error => {
+      console.log(chalk.yellow(`Link con errores, ${link.href} ${error}`))
+    })
+  })
+};
+
 
 const readingFile = (path) => {
   fs.readFile(path, (err, data) => {
